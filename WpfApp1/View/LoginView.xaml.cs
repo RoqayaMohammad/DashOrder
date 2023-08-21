d6c2832e2910167a4031a534d297172e0a10b322
roqaya muhammed <roqayamuhammedrayan@gmail.com>
2023-08-21 23:07:08 +0300

    LOGIN UI

--- WpfApp1/View/LoginView.xaml.cs
+++ WpfApp1/View/LoginView.xaml.cs
@@ -29,5 +29,20 @@ namespace WpfApp1.View
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
