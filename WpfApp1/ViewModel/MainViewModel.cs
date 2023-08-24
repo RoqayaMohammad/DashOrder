1caee8b79c20ca00dea1299d8778aa80c90302ad
roqaya muhammed <roqayamuhammedrayan@gmail.com>
2023-08-24 21:40:31 +0300

    adding resorce dictionary of colors list

--- WpfApp1/ViewModel/MainViewModel.cs
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WpfApp1.Model;
+using WpfApp1.Repositories;
 
 namespace WpfApp1.ViewModel
 {
-    internal class MainViewModel
+    public class MainViewModel: ViewModelBase
     {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private readonly IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+               
+            }
+        }
     }
 }
